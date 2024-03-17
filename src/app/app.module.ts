2dd970d92b3d1f9b1355df98e345d3da2453ed2e
Alice Sales <avss2@cin.ufpe.br>
2024-03-17 19:20:57 -0300

    Ajusta questoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,12 +15,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
   HttpClientModule,
   ImageUploadModalModule,
-  ServiceWorkerModule.register('ngsw-worker.js', {
-    enabled: !isDevMode(),
-    // Register the ServiceWorker as soon as the application is stable
-    // or after 30 seconds (whichever comes first).
-    registrationStrategy: 'registerWhenStable:30000'
-  })],
+  ServiceWorkerModule.register('../ngsw-config.json')],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
